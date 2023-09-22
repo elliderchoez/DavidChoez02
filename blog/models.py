3c3696821ac8536ebfb4fcc984de47cb6650d8b9
elliderchoez <elliderchoez@gmail.com>
2023-09-22 10:33:29 -0500

    primer commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import datetime
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to='blog/images')
+    date=models.DateField(datetime.date.today)
+
+
+class Post1(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    image=models.ImageField(upload_to='blog/images')
+    date=models.DateField(datetime.date.today)
